699cf241eecfeec587337772954b834729fbc2a0
GuilhermeGZanetti <guigoes8@hotmail.com>
2023-05-23 19:29:04 +0200

    Init privacy policy

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PolicyPage = () => {
+  return (
+    <div>
+      <h1>Portfolio Goes</h1>
+      <div>
+        Yay!
+      </div>
+    </div>
+  );
+};
+
+export default PolicyPage;
